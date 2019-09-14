d339f03d5b31f50bd71a7842cb6f815cda4f9696
rsgtechlabs <rsgtechlabs@gmail.com>
2019-09-14 16:30:46 +0530

    Initial Commit

---
+++ src/main/java/com/rsgbank/services/RsgBankCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rsgbank.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsgBankCustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsgBankCustomerServiceApplication.class, args);
+	}
+
+}
